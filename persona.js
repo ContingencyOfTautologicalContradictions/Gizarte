1d50610fc09d4930fabe7a0bb90f39712c1c0723
raygardnercpp <samuelalonso26@icloud.com>
2024-05-23 12:16:19 +0100

    third commit

--- persona.js
+++ persona.js
@@ -8,6 +8,26 @@ let advanced_data = document.getElementById("advanced-data");
 let advanced_data_elements = document.getElementById("advanced-data-elements");
 let observations = document.getElementById("observations");
 let observations_elements = document.getElementById("observations-elements");
+let person = document.getElementById("person");
+let person_fields = document.getElementById("person-fields");
+let home_and_family = document.getElementById("home-and-family");
+let home_and_family_fields = document.getElementById("home-and-family-fields");
+let cohabitation_home = document.getElementById("cohabitation-home");
+let cohabitation_home_elements = document.getElementById("cohabitation-home-elements");
+let registered_address = document.getElementById("registered-address");
+let registered_address_elements = document.getElementById("registered-address-elements");
+let notification_address = document.getElementById("notification-address");
+let notification_address_elements = document.getElementById("notification-address-elements");
+let social_record = document.getElementById("social-record");
+let social_record_fields = document.getElementById("social-record-fields");
+let complementary_personal_data = document.getElementById("complementary-personal-data");
+let complementary_personal_data_elements = document.getElementById("complementary-personal-data-elements");
+let socio_biographical_data = document.getElementById("socio-biographical-data");
+let socio_biographical_data_elements = document.getElementById("socio-biographical-data-elements");
+let reason_for_attention_and_evaluation = document.getElementById("reason-for-attention-and-evaluation");
+let reason_for_attention_and_evaluation_elements = document.getElementById("reason-for-attention-and-evaluation-elements");
+let observations1 = document.getElementById("observations1");
+let observations1_elements = document.getElementById("observations1-elements");
 
 if(censo_personas_link)
     censo_personas_link.addEventListener("click", function(e)
@@ -49,4 +69,111 @@ if(observations)
         personal_data_elements.style.display = "none";
         advanced_data_elements.style.display = "none";
         observations_elements.style.display = "inline";
+    });
+
+if(person)
+    person.addEventListener("click", function()
+    {
+        home_and_family_fields.style.display = "none";
+        cohabitation_home_elements.style.display = "none";
+        registered_address_elements.style.display = "none";
+        notification_address_elements.style.display = "none";
+        social_record_fields.style.display = "none";
+        complementary_personal_data_elements.style.display = "none";
+        socio_biographical_data_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "none";
+        observations1_elements.style.display = "none";
+        person_fields.style.display = "flex";
+        personal_data_elements.style.display = "inline";
+    });
+
+if(home_and_family)
+    home_and_family.addEventListener("click", function()
+    {
+        person_fields.style.display = "none";
+        personal_data_elements.style.display = "none";
+        advanced_data_elements.style.display = "none";
+        observations_elements.style.display = "none";
+        social_record_fields.style.display = "none";
+        complementary_personal_data_elements.style.display = "none";
+        socio_biographical_data_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "none";
+        observations1_elements.style.display = "none";
+        home_and_family_fields.style.display = "flex";
+        cohabitation_home_elements.style.display = "inline";
+    });
+
+if(cohabitation_home)
+    cohabitation_home.addEventListener("click", function()
+    {
+        registered_address_elements.style.display = "none";
+        notification_address_elements.style.display = "none";
+        cohabitation_home_elements.style.display = "inline";
+    });
+
+if(registered_address)
+    registered_address.addEventListener("click", function()
+    {
+        cohabitation_home_elements.style.display = "none";
+        notification_address_elements.style.display = "none";
+        registered_address_elements.style.display = "inline";
+    });
+
+if(notification_address)
+    notification_address.addEventListener("click", function()
+    {
+        cohabitation_home_elements.style.display = "none";
+        registered_address_elements.style.display = "none";
+        notification_address_elements.style.display = "inline";
+    });
+
+if(social_record)
+    social_record.addEventListener("click", function()
+    {
+        person_fields.style.display = "none";
+        personal_data_elements.style.display = "none";
+        advanced_data_elements.style.display = "none";
+        observations_elements.style.display = "none";
+        home_and_family_fields.style.display = "none";
+        cohabitation_home_elements.style.display = "none";
+        registered_address_elements.style.display = "none";
+        notification_address_elements.style.display = "none";
+        social_record_fields.style.display = "flex";
+        complementary_personal_data_elements.style.display = "inline";
+    });
+
+if(complementary_personal_data)
+    complementary_personal_data.addEventListener("click", function()
+    {
+        socio_biographical_data_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "none";
+        observations1_elements.style.display = "none";
+        complementary_personal_data_elements.style.display = "inline";
+    });
+
+if(socio_biographical_data)
+    socio_biographical_data.addEventListener("click", function()
+    {
+        complementary_personal_data_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "none";
+        observations1_elements.style.display = "none";
+        socio_biographical_data_elements.style.display = "inline";
+    });
+
+if(reason_for_attention_and_evaluation)
+    reason_for_attention_and_evaluation.addEventListener("click", function()
+    {
+        complementary_personal_data_elements.style.display = "none";
+        socio_biographical_data_elements.style.display = "none";
+        observations1_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "inline";
+    });
+
+if(observations1)
+    observations1.addEventListener("click", function()
+    {
+        complementary_personal_data_elements.style.display = "none";
+        socio_biographical_data_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "none";
+        observations1_elements.style.display = "inline";
     });
