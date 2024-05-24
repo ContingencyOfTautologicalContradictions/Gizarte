7b8c3bb3d99e1a23019bea2c0f9957632fc111d3
raygardnercpp <samuelalonso26@icloud.com>
2024-05-24 12:29:22 +0100

    fourth commit

--- persona.js
+++ persona.js
@@ -28,6 +28,25 @@ let reason_for_attention_and_evaluation = document.getElementById("reason-for-at
 let reason_for_attention_and_evaluation_elements = document.getElementById("reason-for-attention-and-evaluation-elements");
 let observations1 = document.getElementById("observations1");
 let observations1_elements = document.getElementById("observations1-elements");
+let social_history = document.getElementById("social-history");
+let social_history_fields = document.getElementById("social-history-fields");
+let interventions = document.getElementById("interventions");
+let interventions_elements = document.getElementById("interventions-elements");
+let aes1 = document.getElementById("aes1");
+let aes_elements = document.getElementById("aes-elements");
+let documentation = document.getElementById("documentation");
+
+function remove_if_exists(element, name)
+{
+    if(element.classList.contains(name))
+        element.classList.remove(name);
+}
+
+function add_if_not_exists(element, name)
+{
+    if(!element.classList.contains(name))
+        element.classList.add(name);
+}
 
 if(censo_personas_link)
     censo_personas_link.addEventListener("click", function(e)
@@ -50,6 +69,12 @@ if(change_history_close)
 if(personal_data)
     personal_data.addEventListener("click", function(e)
     {
+        remove_if_exists(advanced_data, "selected-person-control-option");
+        add_if_not_exists(advanced_data, "person-control-option");
+        remove_if_exists(observations, "selected-person-control-option");
+        add_if_not_exists(observations, "person-control-option");
+        remove_if_exists(personal_data, "person-control-option");
+        add_if_not_exists(personal_data, "selected-person-control-option");
         advanced_data_elements.style.display = "none";
         observations_elements.style.display = "none";
         personal_data_elements.style.display = "inline";
@@ -58,6 +83,12 @@ if(personal_data)
 if(advanced_data)
     advanced_data.addEventListener("click", function(e)
     {
+        remove_if_exists(personal_data, "selected-person-control-option");
+        add_if_not_exists(personal_data, "person-control-option");
+        remove_if_exists(observations, "selected-person-control-option");
+        add_if_not_exists(observations, "person-control-option");
+        remove_if_exists(advanced_data, "person-control-option");
+        add_if_not_exists(advanced_data, "selected-person-control-option");
         personal_data_elements.style.display = "none";
         observations_elements.style.display = "none";
         advanced_data_elements.style.display = "inline";
@@ -66,6 +97,12 @@ if(advanced_data)
 if(observations)
     observations.addEventListener("click", function(e)
     {
+        remove_if_exists(personal_data, "selected-person-control-option");
+        add_if_not_exists(personal_data, "person-control-option");
+        remove_if_exists(advanced_data, "selected-person-control-option");
+        add_if_not_exists(advanced_data, "person-control-option");
+        remove_if_exists(observations, "person-control-option");
+        add_if_not_exists(observations, "selected-person-control-option");
         personal_data_elements.style.display = "none";
         advanced_data_elements.style.display = "none";
         observations_elements.style.display = "inline";
@@ -74,6 +111,22 @@ if(observations)
 if(person)
     person.addEventListener("click", function()
     {
+        remove_if_exists(home_and_family, "selected-person-control");
+        add_if_not_exists(home_and_family, "person-control");
+        remove_if_exists(social_record, "selected-person-control");
+        add_if_not_exists(social_record, "person-control");
+        remove_if_exists(social_history, "selected-person-control");
+        add_if_not_exists(social_history, "person-control");
+        remove_if_exists(documentation, "selected-person-control");
+        add_if_not_exists(documentation, "person-control");
+        remove_if_exists(person, "person-control");
+        add_if_not_exists(person, "selected-person-control");
+        remove_if_exists(advanced_data, "selected-person-control-option");
+        add_if_not_exists(advanced_data, "person-control-option");
+        remove_if_exists(observations, "selected-person-control-option");
+        add_if_not_exists(observations, "person-control-option");
+        remove_if_exists(personal_data, "person-control-option");
+        add_if_not_exists(personal_data, "selected-person-control-option");
         home_and_family_fields.style.display = "none";
         cohabitation_home_elements.style.display = "none";
         registered_address_elements.style.display = "none";
@@ -83,6 +136,9 @@ if(person)
         socio_biographical_data_elements.style.display = "none";
         reason_for_attention_and_evaluation_elements.style.display = "none";
         observations1_elements.style.display = "none";
+        social_history_fields.style.display = "none";
+        interventions_elements.style.display = "none";
+        aes_elements.style.display = "none";
         person_fields.style.display = "flex";
         personal_data_elements.style.display = "inline";
     });
@@ -90,6 +146,22 @@ if(person)
 if(home_and_family)
     home_and_family.addEventListener("click", function()
     {
+        remove_if_exists(person, "selected-person-control");
+        add_if_not_exists(person, "person-control");
+        remove_if_exists(social_record, "selected-person-control");
+        add_if_not_exists(social_record, "person-control");
+        remove_if_exists(social_history, "selected-person-control");
+        add_if_not_exists(social_history, "person-control");
+        remove_if_exists(documentation, "selected-person-control");
+        add_if_not_exists(documentation, "person-control");
+        remove_if_exists(home_and_family, "person-control");
+        add_if_not_exists(home_and_family, "selected-person-control");
+        remove_if_exists(registered_address, "selected-person-control-option");
+        add_if_not_exists(registered_address, "person-control-option");
+        remove_if_exists(notification_address, "selected-person-control-option");
+        add_if_not_exists(notification_address, "person-control-option");
+        remove_if_exists(cohabitation_home, "person-control-option");
+        add_if_not_exists(cohabitation_home, "selected-person-control-option");
         person_fields.style.display = "none";
         personal_data_elements.style.display = "none";
         advanced_data_elements.style.display = "none";
@@ -99,6 +171,9 @@ if(home_and_family)
         socio_biographical_data_elements.style.display = "none";
         reason_for_attention_and_evaluation_elements.style.display = "none";
         observations1_elements.style.display = "none";
+        social_history_fields.style.display = "none";
+        interventions_elements.style.display = "none";
+        aes_elements.style.display = "none";
         home_and_family_fields.style.display = "flex";
         cohabitation_home_elements.style.display = "inline";
     });
@@ -106,6 +181,12 @@ if(home_and_family)
 if(cohabitation_home)
     cohabitation_home.addEventListener("click", function()
     {
+        remove_if_exists(registered_address, "selected-person-control-option");
+        add_if_not_exists(registered_address, "person-control-option");
+        remove_if_exists(notification_address, "selected-person-control-option");
+        add_if_not_exists(notification_address, "person-control-option");
+        remove_if_exists(cohabitation_home, "person-control-option");
+        add_if_not_exists(cohabitation_home, "selected-person-control-option");
         registered_address_elements.style.display = "none";
         notification_address_elements.style.display = "none";
         cohabitation_home_elements.style.display = "inline";
@@ -114,6 +195,12 @@ if(cohabitation_home)
 if(registered_address)
     registered_address.addEventListener("click", function()
     {
+        remove_if_exists(cohabitation_home, "selected-person-control-option");
+        add_if_not_exists(cohabitation_home, "person-control-option");
+        remove_if_exists(notification_address, "selected-person-control-option");
+        add_if_not_exists(notification_address, "person-control-option");
+        remove_if_exists(registered_address, "person-control-option");
+        add_if_not_exists(registered_address, "selected-person-control-option");
         cohabitation_home_elements.style.display = "none";
         notification_address_elements.style.display = "none";
         registered_address_elements.style.display = "inline";
@@ -122,6 +209,12 @@ if(registered_address)
 if(notification_address)
     notification_address.addEventListener("click", function()
     {
+        remove_if_exists(cohabitation_home, "selected-person-control-option");
+        add_if_not_exists(cohabitation_home, "person-control-option");
+        remove_if_exists(registered_address, "selected-person-control-option");
+        add_if_not_exists(registered_address, "person-control-option");
+        remove_if_exists(notification_address, "person-control-option");
+        add_if_not_exists(notification_address, "selected-person-control-option");
         cohabitation_home_elements.style.display = "none";
         registered_address_elements.style.display = "none";
         notification_address_elements.style.display = "inline";
@@ -130,6 +223,24 @@ if(notification_address)
 if(social_record)
     social_record.addEventListener("click", function()
     {
+        remove_if_exists(person, "selected-person-control");
+        add_if_not_exists(person, "person-control");
+        remove_if_exists(home_and_family, "selected-person-control");
+        add_if_not_exists(home_and_family, "person-control");
+        remove_if_exists(social_history, "selected-person-control");
+        add_if_not_exists(social_history, "person-control");
+        remove_if_exists(documentation, "selected-person-control");
+        add_if_not_exists(documentation, "person-control");
+        remove_if_exists(social_record, "person-control");
+        add_if_not_exists(social_record, "selected-person-control");
+        remove_if_exists(socio_biographical_data, "selected-person-control-option");
+        add_if_not_exists(socio_biographical_data, "person-control-option");
+        remove_if_exists(reason_for_attention_and_evaluation, "selected-person-control-option");
+        add_if_not_exists(reason_for_attention_and_evaluation, "person-control-option");
+        remove_if_exists(observations1, "selected-person-control-option");
+        add_if_not_exists(observations1, "person-control-option");
+        remove_if_exists(complementary_personal_data, "person-control-option");
+        add_if_not_exists(complementary_personal_data, "selected-person-control-option");
         person_fields.style.display = "none";
         personal_data_elements.style.display = "none";
         advanced_data_elements.style.display = "none";
@@ -138,6 +249,9 @@ if(social_record)
         cohabitation_home_elements.style.display = "none";
         registered_address_elements.style.display = "none";
         notification_address_elements.style.display = "none";
+        social_history_fields.style.display = "none";
+        interventions_elements.style.display = "none";
+        aes_elements.style.display = "none";
         social_record_fields.style.display = "flex";
         complementary_personal_data_elements.style.display = "inline";
     });
@@ -145,6 +259,14 @@ if(social_record)
 if(complementary_personal_data)
     complementary_personal_data.addEventListener("click", function()
     {
+        remove_if_exists(socio_biographical_data, "selected-person-control-option");
+        add_if_not_exists(socio_biographical_data, "person-control-option");
+        remove_if_exists(reason_for_attention_and_evaluation, "selected-person-control-option");
+        add_if_not_exists(reason_for_attention_and_evaluation, "person-control-option");
+        remove_if_exists(observations1, "selected-person-control-option");
+        add_if_not_exists(observations1, "person-control-option");
+        remove_if_exists(complementary_personal_data, "person-control-option");
+        add_if_not_exists(complementary_personal_data, "selected-person-control-option");
         socio_biographical_data_elements.style.display = "none";
         reason_for_attention_and_evaluation_elements.style.display = "none";
         observations1_elements.style.display = "none";
@@ -154,6 +276,14 @@ if(complementary_personal_data)
 if(socio_biographical_data)
     socio_biographical_data.addEventListener("click", function()
     {
+        remove_if_exists(complementary_personal_data, "selected-person-control-option");
+        add_if_not_exists(complementary_personal_data, "person-control-option");
+        remove_if_exists(reason_for_attention_and_evaluation, "selected-person-control-option");
+        add_if_not_exists(reason_for_attention_and_evaluation, "person-control-option");
+        remove_if_exists(observations1, "selected-person-control-option");
+        add_if_not_exists(observations1, "person-control-option");
+        remove_if_exists(socio_biographical_data, "person-control-option");
+        add_if_not_exists(socio_biographical_data, "selected-person-control-option");
         complementary_personal_data_elements.style.display = "none";
         reason_for_attention_and_evaluation_elements.style.display = "none";
         observations1_elements.style.display = "none";
@@ -163,6 +293,14 @@ if(socio_biographical_data)
 if(reason_for_attention_and_evaluation)
     reason_for_attention_and_evaluation.addEventListener("click", function()
     {
+        remove_if_exists(complementary_personal_data, "selected-person-control-option");
+        add_if_not_exists(complementary_personal_data, "person-control-option");
+        remove_if_exists(socio_biographical_data, "selected-person-control-option");
+        add_if_not_exists(socio_biographical_data, "person-control-option");
+        remove_if_exists(observations1, "selected-person-control-option");
+        add_if_not_exists(observations1, "person-control-option");
+        remove_if_exists(reason_for_attention_and_evaluation, "person-control-option");
+        add_if_not_exists(reason_for_attention_and_evaluation, "selected-person-control-option");
         complementary_personal_data_elements.style.display = "none";
         socio_biographical_data_elements.style.display = "none";
         observations1_elements.style.display = "none";
@@ -172,8 +310,72 @@ if(reason_for_attention_and_evaluation)
 if(observations1)
     observations1.addEventListener("click", function()
     {
+        remove_if_exists(complementary_personal_data, "selected-person-control-option");
+        add_if_not_exists(complementary_personal_data, "person-control-option");
+        remove_if_exists(socio_biographical_data, "selected-person-control-option");
+        add_if_not_exists(socio_biographical_data, "person-control-option");
+        remove_if_exists(reason_for_attention_and_evaluation, "selected-person-control-option");
+        add_if_not_exists(reason_for_attention_and_evaluation, "person-control-option");
+        remove_if_exists(observations1, "person-control-option");
+        add_if_not_exists(observations1, "selected-person-control-option");
         complementary_personal_data_elements.style.display = "none";
         socio_biographical_data_elements.style.display = "none";
         reason_for_attention_and_evaluation_elements.style.display = "none";
         observations1_elements.style.display = "inline";
+    });
+
+if(social_history)
+    social_history.addEventListener("click", function()
+    {
+        remove_if_exists(person, "selected-person-control");
+        add_if_not_exists(person, "person-control");
+        remove_if_exists(home_and_family, "selected-person-control");
+        add_if_not_exists(home_and_family, "person-control");
+        remove_if_exists(social_record, "selected-person-control");
+        add_if_not_exists(social_record, "person-control");
+        remove_if_exists(documentation, "selected-person-control");
+        add_if_not_exists(documentation, "person-control");
+        remove_if_exists(social_history, "person-control");
+        add_if_not_exists(social_history, "selected-person-control");
+        remove_if_exists(aes1, "selected-person-control-option");
+        add_if_not_exists(aes1, "person-control-option");
+        remove_if_exists(interventions, "person-control-option");
+        add_if_not_exists(interventions, "selected-person-control-option");
+        person_fields.style.display = "none";
+        personal_data_elements.style.display = "none";
+        advanced_data_elements.style.display = "none";
+        observations_elements.style.display = "none";
+        home_and_family_fields.style.display = "none";
+        cohabitation_home_elements.style.display = "none";
+        registered_address_elements.style.display = "none";
+        notification_address_elements.style.display = "none";
+        social_record_fields.style.display = "none";
+        complementary_personal_data_elements.style.display = "none";
+        socio_biographical_data_elements.style.display = "none";
+        reason_for_attention_and_evaluation_elements.style.display = "none";
+        observations1.style.display = "none";
+        social_history_fields.style.display = "flex";
+        interventions_elements.style.display = "inline";
+    });
+
+if(interventions)
+    interventions.addEventListener("click", function()
+    {
+        remove_if_exists(aes1, "selected-person-control-option");
+        add_if_not_exists(aes1, "person-control-option");
+        remove_if_exists(interventions, "person-control-option");
+        add_if_not_exists(interventions, "selected-person-control-option");
+        aes_elements.style.display = "none";
+        interventions_elements.style.display = "inline";
+    });
+
+if(aes1)
+    aes1.addEventListener("click", function()
+    {
+        remove_if_exists(interventions, "selected-person-control-option");
+        add_if_not_exists(interventions, "person-control-option");
+        remove_if_exists(aes1, "person-control-option");
+        add_if_not_exists(aes1, "selected-person-control-option");
+        interventions_elements.style.display = "none";
+        aes_elements.style.display = "inline";
     });
